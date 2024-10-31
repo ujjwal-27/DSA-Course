1e8957dd0eb442a6e9179edc79cea37b3beee7e8
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-10-31 13:44:36 +0545

    perform simple for-loop task

---
+++ src/bigO/oOfN.java
@@ -0,0 +1,13 @@
+package bigO;
+
+public class oOfN {
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+}
