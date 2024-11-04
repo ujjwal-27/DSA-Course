4714b3bb4e4229787cdb592e8496ef884c86b519
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-11-04 12:37:32 +0545

    Big O: O of n square concept

---
+++ src/bigO/oOfNSquare.java
@@ -0,0 +1,20 @@
+package bigO;
+
+/**
+ * Here, we have n = 10, and no of operations = n * n = n^2
+ * Hence, it is referred as O(n^2).
+ */
+public class oOfNSquare {
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
+    public static void printItems(int n) {
+        // nested for loop
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+    }
+}
