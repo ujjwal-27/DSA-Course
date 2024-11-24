4cf471a7574c71cd53d5a05a49b17eb6e8dfcf3d
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-11-24 16:32:44 +0545

    implement node and linked list constructors

---
+++ src/datastructures/linkedLists/LinkedList.java
@@ -0,0 +1,24 @@
+package datastructures.linkedLists;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+}
---
+++ src/datastructures/linkedLists/Main.java
@@ -0,0 +1,8 @@
+package datastructures.linkedLists;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(4);
+        System.out.println(linkedList);
+    }
+}
