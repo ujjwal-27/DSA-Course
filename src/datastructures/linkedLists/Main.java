7d78a0c327f561f43b3a517e39da08fb18c54ab3
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-11-26 14:01:48 +0545

    invoke and test removeFirst method

--- src/datastructures/linkedLists/Main.java
+++ src/datastructures/linkedLists/Main.java
@@ -3,15 +3,15 @@ package datastructures.linkedLists;
 public class Main {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList(4);
-        linkedList.prepend(27);
-        linkedList.prepend(7);
-        linkedList.prepend(2);
+        linkedList.append(27);
+        linkedList.append(7);
+
+        System.out.println("removed: " + linkedList.removeFirst().value);
+        System.out.println("removed: " + linkedList.removeFirst().value);
+        System.out.println("removed: " + linkedList.removeFirst().value);
+        System.out.println(linkedList.removeFirst());
 
         linkedList.printList();
-
-//        System.out.println(linkedList.removeLast().value);
-//        System.out.println(linkedList.removeLast().value);
-//        System.out.println(linkedList.removeLast());
 
         linkedList.getHead();
         linkedList.getTail();
