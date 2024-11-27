d6542c0b09e16951c02f691c98146b821f158e18
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-11-27 13:15:37 +0545

    invoke and test insert method

--- src/datastructures/linkedLists/Main.java
+++ src/datastructures/linkedLists/Main.java
@@ -7,8 +7,11 @@ public class Main {
         linkedList.append(7);
         linkedList.append(2);
 
-        linkedList.insert(3, 8);
+
+        linkedList.insert(0, 8);
+        linkedList.insert(3, 57);
         linkedList.insert(5, 9);
+        linkedList.insert(7, 11);
 
         linkedList.printList();
     }
