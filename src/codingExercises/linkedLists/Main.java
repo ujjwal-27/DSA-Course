4a85122d7ec01e47f7ee4ea6fe8a47f6fbed1baf
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-01 16:40:50 +0545

    initial commit for get method

--- src/codingExercises/linkedLists/Main.java
+++ src/codingExercises/linkedLists/Main.java
@@ -1,47 +1,23 @@
 package codingExercises.linkedLists;
 
 public class Main {
-
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(2);
+
+        LinkedList myLinkedList = new LinkedList(0);
+        myLinkedList.append(1);
+        myLinkedList.append(2);
         myLinkedList.append(3);
 
-        System.out.println("Before prepend():");
-        System.out.println("-----------------");
-        myLinkedList.printAll();
 
-        myLinkedList.prepend(1);
-
-        System.out.println("\n\nAfter prepend():");
-        System.out.println("----------------");
-        myLinkedList.printAll();
+        System.out.println(myLinkedList.get(3).value);
 
 
         /*
             EXPECTED OUTPUT:
-
-            Before prepend():
-            -----------------
-            Head: 2
-            Tail: 3
-            Length: 2
-
-            Linked List:
-            2
-            3
-
-
-            After prepend():
             ----------------
-            Head: 1
-            Tail: 3
-            Length: 3
-
-            Linked List:
-            1
-            2
             3
 
         */
+
     }
 }
