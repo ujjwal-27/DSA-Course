0f58d351f8aed6a5e93d81956d16e9fb0550ba34
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-01 16:53:43 +0545

    create set method

--- src/codingExercises/linkedLists/LinkedList.java
+++ src/codingExercises/linkedLists/LinkedList.java
@@ -145,6 +145,17 @@ public class LinkedList {
 
         length++;
     }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+
+        return false;
+    }
 }
 
 
