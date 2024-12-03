dee638f6f083955af7a34f910ed5be9f240d7316
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-03 14:23:30 +0545

    implement findMiddleNode method using hare-tortoise algorithm

--- src/interviewQuestions/linkedList/LinkedList.java
+++ src/interviewQuestions/linkedList/LinkedList.java
@@ -67,12 +67,25 @@ public class LinkedList {
         }
     }
 
-    // WRITE FIND MIDDLE NODE METHOD HERE //
-    //                                    //
-    //                                    //
-    //                                    //
-    //                                    //
-    ////////////////////////////////////////
+    /**
+     * This method implements hare-tortoise algorithm (Floyd's Tortoise and Hare Algorithm) to find the middle node.
+     * Here, two pointers - slow and fast - are initialized, both pointing to the head of the list.
+     * While-loop is implemented with the condition stating that the loop shall execute until 'fast' is not null and 'next node of fast' is not null.
+     * Inside the code block of while loop, move 'slow' pointer one step ahead, and 'fast' pointer two steps ahead.
+     * When the while loop ends, the 'slow' pointer will point to the middle Node of the list.
+     * @return Slow pointer
+     */
+    public Node findMiddleNode() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) { // fast != null --> for even no. of elements AND fast.next != null --> for odd no. of elements in the list
+            slow = slow.next; // one step ahead
+            fast = fast.next.next; // two steps ahead
+        }
+
+        return slow; 
+    }
 }
 
 
