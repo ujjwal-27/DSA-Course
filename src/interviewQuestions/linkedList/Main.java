10b643be58d89b0e64ff332efb226c9c39451591
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-05 14:03:24 +0545

    revert code

--- src/interviewQuestions/linkedList/Main.java
+++ src/interviewQuestions/linkedList/Main.java
@@ -2,42 +2,21 @@ package interviewQuestions.linkedList;
 
 public class Main {
     public static void main(String[] args) {
-        // Create a new LinkedList and append values to it
-        LinkedList ll = new LinkedList(3);
-        ll.append(5);
-        ll.append(8);
-        ll.append(10);
-        ll.append(2);
-        ll.append(1);
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
 
-        // Print the list before partitioning
-        System.out.println("LL before partitionList:");
-        ll.printList(); // Output: 3 5 8 10 2 1
+        int k = 2;
+        int result = myLinkedList.findKthFromEnd(k).value;
 
-        // Call the partitionList method with x = 5
-        ll.partitionList(5);
-
-        // Print the list after partitioning
-        System.out.println("LL after partitionList:");
-        ll.printList(); // Output: 3 2 1 5 8 10
+        System.out.println(result); // Output: 4
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            LL before partition_list:
-            3
-            5
-            8
-            10
-            2
-            1
-            LL after partition_list:
-            3
-            2
-            1
-            5
-            8
-            10
+            4
 
         */
     }
