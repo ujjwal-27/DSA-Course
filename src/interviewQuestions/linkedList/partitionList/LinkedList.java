0b25e5dc2c019ffd290006bfae3124637cbc666b
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-09 12:06:44 +0545

    implement partitionList method (course-based logic)

--- src/interviewQuestions/linkedList/partitionList/LinkedList.java
+++ src/interviewQuestions/linkedList/partitionList/LinkedList.java
@@ -69,7 +69,7 @@ public class LinkedList {
         length++;
     }
 
-    public void partitionList(int x) {
+    public void partitionList_self(int x) {
         if (head != null && length != 1) {
             LinkedList highChain = null; // a new linked list for holding values >= x
             LinkedList lowChain = null; // a new linked list for holding values < x
@@ -106,6 +106,33 @@ public class LinkedList {
             lowTail.next = highHead;
             head = lowHead;
         }
+    }
+
+    public void partitionList(int x) {
+        if (head == null) return;
+
+        Node dummy1 = new Node(0);
+        Node dummy2 = new Node(0);
+        Node prev1 = dummy1;
+        Node prev2 = dummy2;
+        Node current = head;
+
+        while (current != null) {
+            if (current.value < x) {
+                prev1.next = current;
+                prev1 = current;
+
+            } else {
+                prev2.next = current;
+                prev2 = current;
+            }
+
+            current = current.next;
+        }
+
+        prev2.next = null;
+        prev1.next = dummy2.next;
+        head = dummy1.next;
     }
 }
 
