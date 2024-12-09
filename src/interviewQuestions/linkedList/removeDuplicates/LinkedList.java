02b5a385efde538d202eb28c9ebb82fd06ec745e
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-09 12:13:10 +0545

    initial commit for removeDuplicates method

---
+++ src/interviewQuestions/linkedList/removeDuplicates/LinkedList.java
@@ -0,0 +1,84 @@
+package interviewQuestions.linkedList.removeDuplicates;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedList {
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        length++;
+    }
+
+    // WRITE THE REMOVEDUPLICATES METHOD HERE //
+    //                                        //
+    //                                        //
+    //                                        //
+    //                                        //
+    ////////////////////////////////////////////
+
+}
+
+
---
+++ src/interviewQuestions/linkedList/removeDuplicates/Main.java
@@ -0,0 +1,31 @@
+package interviewQuestions.linkedList.removeDuplicates;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(1);
+
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(3);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+
+        myLinkedList.removeDuplicates();
+
+        myLinkedList.printList();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            1
+            2
+            3
+            4
+
+        */
+
+    }
+
+}
