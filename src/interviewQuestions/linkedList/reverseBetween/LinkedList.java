e471c5e752a6bf8345f3ca1c58597897b01972d8
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-10 16:06:36 +0545

    initial commit for reverseBetween method

---
+++ src/interviewQuestions/linkedList/reverseBetween/LinkedList.java
@@ -0,0 +1,80 @@
+package interviewQuestions.linkedList.reverseBetween;
+
+public class LinkedList {
+
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        length++;
+    }
+
+    // WRITE THE REVERSEBETWEEN METHOD HERE //
+    //                                      //
+    //                                      //
+    //                                      //
+    //                                      //
+    //////////////////////////////////////////
+
+}
