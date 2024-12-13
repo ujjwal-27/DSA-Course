ac6ff1c8257b5af6795ed33898f2b1f70df48274
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-13 09:23:05 +0545

    create inner class 'Node'

--- src/datastructures/doublyLinkedList/DoublyLinkedList.java
+++ src/datastructures/doublyLinkedList/DoublyLinkedList.java
@@ -1,5 +1,13 @@
 package datastructures.doublyLinkedList;
 
 public class DoublyLinkedList {
+    class Node {
+        int value;
+        Node next;
+        Node prev;
 
+        Node(int value) {
+            this.value = value;
+        }
+    }
 }
