5bb708152508bb9f6ab2ba95b149d146dfa3e592
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-13 11:17:03 +0545

    create instance variable and constructor

--- src/datastructures/doublyLinkedList/DoublyLinkedList.java
+++ src/datastructures/doublyLinkedList/DoublyLinkedList.java
@@ -1,6 +1,18 @@
 package datastructures.doublyLinkedList;
 
 public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DoublyLinkedList(int value) {
+        Node node = new Node(value);
+
+        head = node;
+        tail = node;
+        length++;
+    }
+
     class Node {
         int value;
         Node next;
