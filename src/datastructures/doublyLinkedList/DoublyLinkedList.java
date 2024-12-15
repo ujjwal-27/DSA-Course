f595ce8265836ad84c82f3992b415559289628a7
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-15 12:35:48 +0545

    implement and test removeFirst method

--- src/datastructures/doublyLinkedList/DoublyLinkedList.java
+++ src/datastructures/doublyLinkedList/DoublyLinkedList.java
@@ -75,6 +75,25 @@ public class DoublyLinkedList {
         length++;
     }
 
+    public Node removeFirst() {
+        if (length == 0) return null;
+
+        Node temp = head;
+
+        if (length == 1) {
+            head = null;
+            tail = null;
+
+        } else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+
+        length--;
+        return temp;
+    }
+
     public Node removeLast() {
         if (length == 0) return null;
 
--- src/datastructures/doublyLinkedList/Main.java
+++ src/datastructures/doublyLinkedList/Main.java
@@ -3,10 +3,17 @@ package datastructures.doublyLinkedList;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList doublyLinkedList = new DoublyLinkedList(7);
-        doublyLinkedList.prepend(2);
-        doublyLinkedList.prepend(27);
-        doublyLinkedList.prepend(22);
+        doublyLinkedList.append(2);
+        doublyLinkedList.append(27);
+        doublyLinkedList.append(99);
 
+        System.out.println("---- Before remove first ----");
+        doublyLinkedList.getLength();
+        doublyLinkedList.getList();
+
+        System.out.println("---- After remove first ----");
+        doublyLinkedList.removeFirst();
+        doublyLinkedList.getLength();
         doublyLinkedList.getList();
     }
 }
