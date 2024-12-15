84085dbbc8d61e48f5c18a841d0bb5361002be2c
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-15 11:34:55 +0545

    update main class

--- src/datastructures/doublyLinkedList/Main.java
+++ src/datastructures/doublyLinkedList/Main.java
@@ -3,21 +3,12 @@ package datastructures.doublyLinkedList;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList doublyLinkedList = new DoublyLinkedList(7);
-        doublyLinkedList.append(2);
-        doublyLinkedList.append(27);
-        doublyLinkedList.append(99);
 
         System.out.println("---- Before remove last ----");
-        doublyLinkedList.getHead();
-        doublyLinkedList.getTail();
-        doublyLinkedList.getLength();
         doublyLinkedList.getList();
 
-        System.out.println("---- Before remove last ----");
+        System.out.println("---- After remove last ----");
         doublyLinkedList.removeLast();
-        doublyLinkedList.getHead();
-        doublyLinkedList.getTail();
-        doublyLinkedList.getLength();
         doublyLinkedList.getList();
     }
 }
