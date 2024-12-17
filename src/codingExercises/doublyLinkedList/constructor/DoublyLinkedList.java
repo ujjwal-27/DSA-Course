0c02ef3f2c01c120473593ecbd6c476f9a8d4713
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-17 13:18:51 +0545

    initial commit for constructor

---
+++ src/codingExercises/doublyLinkedList/constructor/DoublyLinkedList.java
@@ -0,0 +1,51 @@
+package codingExercises.doublyLinkedList.constructor;
+
+public class DoublyLinkedList {
+
+    // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE //
+    //                                                          //
+    //                                                          //
+    //                                                          //
+    //                                                          //
+    //////////////////////////////////////////////////////////////
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nDoubly Linked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+}
+
+
---
+++ src/codingExercises/doublyLinkedList/constructor/Main.java
@@ -0,0 +1,26 @@
+package codingExercises.doublyLinkedList.constructor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+
+        myDLL.printAll();
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Head: 7
+            Tail: 7
+            Length: 1
+
+            Linked List:
+            7
+
+        */
+
+    }
+
+}
