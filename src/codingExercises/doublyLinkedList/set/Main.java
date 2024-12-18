a4c4098b5635a08bffc25a04e30dadb5614aed06
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-18 12:14:05 +0545

    initial commit for set method

---
+++ src/codingExercises/doublyLinkedList/set/Main.java
@@ -0,0 +1,40 @@
+package codingExercises.doublyLinkedList.set;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        myDLL.append(1);
+        myDLL.append(2);
+        myDLL.append(3);
+
+        System.out.println("DLL before set():");
+        myDLL.printList();
+
+        myDLL.set(2, 99);
+
+        System.out.println("\nDLL after set():");
+        myDLL.printList();
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            DLL before set():
+            0
+            1
+            2
+            3
+
+            DLL after set():
+            0
+            1
+            99
+            3
+
+        */
+
+    }
+
+}
