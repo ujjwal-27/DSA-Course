4bccc688f890e0c22014899b101ea5ccc87e8456
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-18 11:48:30 +0545

    implement removeFirst method

--- src/codingExercises/doublyLinkedList/removeFirst/DoublyLinkedList.java
+++ src/codingExercises/doublyLinkedList/removeFirst/DoublyLinkedList.java
@@ -107,11 +107,22 @@ public class DoublyLinkedList {
         length++;
     }
 
-    // WRITE REMOVEFIRST METHOD HERE //
-    //                               //
-    //                               //
-    //                               //
-    //                               //
-    ///////////////////////////////////
+    public Node removeFirst() {
+        if (length == 0) return null;
 
+        Node temp = head;
+
+        if (length == 1) {
+            head = null;
+            tail = null;
+
+        } else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+
+        length--;
+        return temp;
+    }
 }
