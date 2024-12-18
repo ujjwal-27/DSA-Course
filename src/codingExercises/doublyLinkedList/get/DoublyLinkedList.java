b82e0057d5e1d8b357d0650b338a3115114822d3
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-18 12:09:19 +0545

    implement get method

--- src/codingExercises/doublyLinkedList/get/DoublyLinkedList.java
+++ src/codingExercises/doublyLinkedList/get/DoublyLinkedList.java
@@ -122,11 +122,24 @@ public class DoublyLinkedList {
         return temp;
     }
 
-    /// WRITE GET METHOD HERE ///
-    //                         //
-    //                         //
-    //                         //
-    //                         //
-    /////////////////////////////
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
 
+        Node temp = head;
+
+        if (index < length / 2) {
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+
+        } else {
+            temp = tail;
+
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+
+        return temp;
+    }
 }
--- src/codingExercises/doublyLinkedList/get/Main.java
+++ src/codingExercises/doublyLinkedList/get/Main.java
@@ -4,13 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(0);
-        myDLL.append(1);
-        myDLL.append(2);
-        myDLL.append(3);
+        DoublyLinkedList myDLL = new DoublyLinkedList(22);
+        myDLL.append(7);
+        myDLL.append(27);
+        myDLL.append(77);
+        myDLL.append(7);
 
 
-        System.out.println(myDLL.get(3).value);
+        System.out.println(myDLL.get(2).value);
 
 
         /*
