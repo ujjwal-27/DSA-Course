3011cb234eb3a8ffbe1cfa521fb803f385d0eb26
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-24 13:39:02 +0545

    create Node class and constructor

--- src/datastructures/stack/Stack.java
+++ src/datastructures/stack/Stack.java
@@ -1,4 +1,12 @@
 package datastructures.stack;
 
 public class Stack {
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
 }
