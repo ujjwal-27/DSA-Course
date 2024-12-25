497404c5e48e5ab4d5d75e4cdc6d637a9dd32262
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-25 14:57:21 +0545

    implement dequeue method

--- src/codingExercises/queue/dequeue/Queue.java
+++ src/codingExercises/queue/dequeue/Queue.java
@@ -77,13 +77,23 @@ public class Queue {
         length++;
     }
 
-    // WRITE DEQUEUE METHOD HERE //
-    //                           //
-    //                           //
-    //                           //
-    //                           //
-    ///////////////////////////////
+    public Node dequeue() {
+        if (length == 0) return null;
 
+        Node temp = first;
+
+        if (length == 1) {
+            first = null;
+            last = null;
+
+        } else {
+            first = first.next;
+            temp.next = null;
+        }
+
+        length--;
+        return temp;
+    }
 }
 
 
