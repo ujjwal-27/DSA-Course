8a6ad809fa9e90a5428ec039a05329a55110850d
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-25 13:43:58 +0545

    update and test dequeue method

--- src/datastructures/queue/Main.java
+++ src/datastructures/queue/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Queue queue = new Queue(7);
 
+        queue.enqueue(72);
+        queue.enqueue(2);
+        queue.enqueue(22);
+
         queue.getFirst();
         queue.getLast();
         queue.getLength();
--- src/datastructures/queue/Queue.java
+++ src/datastructures/queue/Queue.java
@@ -23,11 +23,21 @@ public class Queue {
     }
 
     public Node dequeue() {
-        if (length == 0) return null;
+        if (length == 0) {
+            return null;
+        }
 
         Node temp = first;
-        first = first.next;
-        temp.next = null;
+
+        if (length == 1) {
+            first = null;
+            last = null;
+
+        } else {
+            first = first.next;
+            temp.next = null;
+        }
+
         length--;
 
         return temp;
