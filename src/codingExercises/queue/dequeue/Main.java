08b2bf85de337123b40e84c774a3b1910d6aa906
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-25 14:49:20 +0545

    initial commit for dequeue method

---
+++ src/codingExercises/queue/dequeue/Main.java
@@ -0,0 +1,29 @@
+package codingExercises.queue.dequeue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Queue myQueue = new Queue(2);
+        myQueue.enqueue(1);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myQueue.dequeue().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myQueue.dequeue().value);
+        // (0) Items - Returns null
+        System.out.println(myQueue.dequeue());
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            2
+            1
+            null
+
+        */
+
+    }
+
+}
