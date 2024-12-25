43f4a37531f30fd03fa5c365833050abb3c61c9f
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-25 13:08:51 +0545

    create Node class and constructor

--- src/datastructures/queue/Queue.java
+++ src/datastructures/queue/Queue.java
@@ -1,4 +1,12 @@
 package datastructures.queue;
 
 public class Queue {
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
 }
