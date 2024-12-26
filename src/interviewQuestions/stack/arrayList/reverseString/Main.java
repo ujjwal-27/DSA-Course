69c15a63090a0af2259278437bc2675a25349b32
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-26 13:49:04 +0545

    initial commit for stack reverseString method

---
+++ src/interviewQuestions/stack/arrayList/reverseString/Main.java
@@ -0,0 +1,29 @@
+package interviewQuestions.stack.arrayList.reverseString;
+
+public class Main {
+
+    // WRITE THE REVERSESTRING METHOD HERE //
+    //                                     //
+    //                                     //
+    //                                     //
+    //                                     //
+    /////////////////////////////////////////
+
+
+
+    public static void main(String[] args) {
+
+        String myString = "hello";
+        String reversedString = reverseString(myString);
+        System.out.println(reversedString);
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            olleh
+        */
+
+    }
+
+}
+
