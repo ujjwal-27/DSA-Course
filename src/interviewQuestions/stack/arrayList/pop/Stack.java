730742c81b2a464440a67541a4225776a7dc78cc
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-26 12:55:22 +0545

    initial commit for stack arrayList pop method

---
+++ src/interviewQuestions/stack/arrayList/pop/Stack.java
@@ -0,0 +1,46 @@
+package interviewQuestions.stack.arrayList.pop;
+
+import java.util.ArrayList;
+
+public class Stack<T> {
+
+    private ArrayList<T> stackList = new ArrayList<>();
+
+    public ArrayList<T> getStackList() {
+        return stackList;
+    }
+
+    public void printStack() {
+        for (int i = stackList.size()-1; i >= 0; i--) {
+            System.out.println(stackList.get(i));
+        }
+    }
+
+    public boolean isEmpty() {
+        return stackList.size() == 0;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return stackList.get(stackList.size() - 1);
+        }
+    }
+
+    public int size() {
+        return stackList.size();
+    }
+
+    public void push(T value) {
+        stackList.add(value);
+    }
+
+    // WRITE POP METHOD HERE //
+    //                       //
+    //                       //
+    //                       //
+    ///////////////////////////
+
+}
+
