d75f613a4c5ac638cae24f2d7a4e00af08baf0bd
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-26 12:15:06 +0545

    initial commit for stack arrayList push method

---
+++ src/interviewQuestions/stack/arrayList/push/Main.java
@@ -0,0 +1,25 @@
+package interviewQuestions.stack.arrayList.push;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack myStack = new Stack();
+
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+
+        myStack.printStack();
+
+        /*
+            EXPECTED OUTPUT:
+			3
+			2
+			1
+
+        */
+
+    }
+
+}
