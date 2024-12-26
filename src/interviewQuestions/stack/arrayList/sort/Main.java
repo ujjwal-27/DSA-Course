0b2bf0d61c942f6e5536bcba83075c5437a4fd78
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-26 17:32:04 +0545

    initial commit for sortStack method

---
+++ src/interviewQuestions/stack/arrayList/sort/Main.java
@@ -0,0 +1,51 @@
+package interviewQuestions.stack.arrayList.sort;
+
+public class Main {
+
+    // WRITE THE SORTSTACK METHOD HERE //
+    //                                 //
+    //                                 //
+    //                                 //
+    //                                 //
+    /////////////////////////////////////
+
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(1);
+        stack.push(4);
+        stack.push(2);
+
+        System.out.println("Before sorting:");
+        stack.printStack();
+
+        sortStack(stack);
+
+        System.out.println("\nAfter sorting:");
+        stack.printStack();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Before sorting:
+            4
+            1
+            5
+            2
+            3
+
+            After sorting:
+            1
+            2
+            3
+            4
+            5
+
+        */
+
+    }
+
+}
+
+
