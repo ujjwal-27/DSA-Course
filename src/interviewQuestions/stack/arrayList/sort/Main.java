bdadf9386c337cbf36df1462fcc87640477fa9bf
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-27 14:47:09 +0545

    implement sortStack method (self implementation)

--- src/interviewQuestions/stack/arrayList/sort/Main.java
+++ src/interviewQuestions/stack/arrayList/sort/Main.java
@@ -2,20 +2,40 @@ package interviewQuestions.stack.arrayList.sort;
 
 public class Main {
 
-    // WRITE THE SORTSTACK METHOD HERE //
-    //                                 //
-    //                                 //
-    //                                 //
-    //                                 //
-    /////////////////////////////////////
+    public static void sortStack(Stack<Integer> originalStack) {
+        Stack<Integer> sortedStack = new Stack<>();
+
+        while (!originalStack.isEmpty()) {
+            Integer originalTopElement = originalStack.pop();
+            Integer peekSortedStack = sortedStack.peek();
+
+            if (peekSortedStack == null || originalTopElement > peekSortedStack) {
+                sortedStack.push(originalTopElement);
+
+            } else {
+                while (!sortedStack.isEmpty() && originalTopElement < peekSortedStack) {
+                    Integer sortedTopElement = sortedStack.pop();
+                    originalStack.push(sortedTopElement);
+                }
+
+                sortedStack.push(originalTopElement);
+            }
+        }
+
+        while (!sortedStack.isEmpty()) {
+            Integer sortedElement = sortedStack.pop();
+            originalStack.push(sortedElement);
+        }
+    }
 
 
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         stack.push(3);
+        stack.push(2);
+        stack.push(5);
         stack.push(1);
         stack.push(4);
-        stack.push(2);
 
         System.out.println("Before sorting:");
         stack.printStack();
