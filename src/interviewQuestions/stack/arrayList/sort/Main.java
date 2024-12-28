f00a9dd755462a7ddf69db86163ff71bece79d74
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-28 11:43:13 +0545

    optimize sortStack method (hints-followed)

--- src/interviewQuestions/stack/arrayList/sort/Main.java
+++ src/interviewQuestions/stack/arrayList/sort/Main.java
@@ -7,28 +7,22 @@ public class Main {
 
         // According to the question, top element from original stack is to be removed, and compare it with top element of sorted stack.
         // If the top element of the sorted stack is greater than the current top element of original stack, the top element of the sorted stack should be moved back to the original stack until the current top element from original stack is in the correct position.
-        // Following this concept, this loop will execute until original stack is empty.
+        // Following this concept, this outer loop will execute until original stack is empty.
         while (!originalStack.isEmpty()) {
             Integer originalTopElement = originalStack.pop(); // top element from original stack removed, and stored in 'originalTopElement' variable (this is also referred as current element)
             Integer peekSortedStack = sortedStack.peek(); // getting (not removed) top element from sorted stack, and stored in 'peekSortedStack' variable
 
-            // If the sorted stack is empty, or the top element of original stack (current element) is greater than top element of sorted stack, then add the current element to sorted stack
-            if (peekSortedStack == null || originalTopElement > peekSortedStack) {
-                sortedStack.push(originalTopElement);
-
-            // If the current element is smaller than the top element of sorted stack, this code block will execute
-            } else {
-                // This loop will execute until the sorted stack is not empty, and current element is less than top elements of sorted stack from each iteration.
-                while (!sortedStack.isEmpty() && originalTopElement < peekSortedStack) {
-                    // As long as the condition stated in while statement is true, the top element from sorted stack is removed, and added back to original stack.
-                    Integer sortedTopElement = sortedStack.pop();
-                    originalStack.push(sortedTopElement);
-                    peekSortedStack = sortedStack.peek(); // updating the value of 'peekSortedStack' after executing pop method on sorted stack
-                }
-
-                // Once the top element from sorted stack is found which is smaller than current element, it is added to sorted stack
-                sortedStack.push(originalTopElement);
+            // This inner loop will execute until the sorted stack is not empty, and current element is less than top elements of sorted stack from each iteration.
+            // If one of the conditions is not satisfied, current element will be pushed to sorted stack.
+            while (!sortedStack.isEmpty() && originalTopElement < peekSortedStack) {
+                // As long as the condition stated in while statement is true, the top element from sorted stack is removed, and added back to original stack.
+                Integer sortedTopElement = sortedStack.pop();
+                originalStack.push(sortedTopElement);
+                peekSortedStack = sortedStack.peek(); // updating the value of 'peekSortedStack' after executing pop method on sorted stack
             }
+
+            // Once the top element from sorted stack is found which is smaller than current element, it is added to sorted stack
+            sortedStack.push(originalTopElement);
         }
 
         // This loop will execute until sorted stack is empty
