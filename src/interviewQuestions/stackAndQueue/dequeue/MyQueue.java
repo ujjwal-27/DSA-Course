6bb71db6adae8f4c41971aa36447a5089c213790
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-29 13:34:26 +0545

    initial commit for dequeue

---
+++ src/interviewQuestions/stackAndQueue/dequeue/MyQueue.java
@@ -0,0 +1,39 @@
+package interviewQuestions.stackAndQueue.dequeue;
+
+import java.util.Stack;
+
+public class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void enqueue(int value) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        stack1.push(value);
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
+
+    // WRITE THE DEQUEUE METHOD HERE //
+    //                               //
+    //                               //
+    //                               //
+    //                               //
+    ///////////////////////////////////
+
+    public int peek() {
+        return stack1.peek();
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty();
+    }
+
+}
