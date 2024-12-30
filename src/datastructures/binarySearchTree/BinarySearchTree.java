8a9c6c9003881046889cc9988a3fcc42fe4fa933
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-30 21:25:50 +0545

    create Node class and constructor

--- src/datastructures/binarySearchTree/BinarySearchTree.java
+++ src/datastructures/binarySearchTree/BinarySearchTree.java
@@ -1,4 +1,13 @@
 package datastructures.binarySearchTree;
 
 public class BinarySearchTree {
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        private Node(int value) {
+            this.value = value;
+        }
+    }
 }
