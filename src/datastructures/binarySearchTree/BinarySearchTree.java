6f28adb02695af3c25edbd709520eb43588641b2
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-30 21:28:20 +0545

    initialize and print instance variable: root

--- src/datastructures/binarySearchTree/BinarySearchTree.java
+++ src/datastructures/binarySearchTree/BinarySearchTree.java
@@ -1,6 +1,8 @@
 package datastructures.binarySearchTree;
 
 public class BinarySearchTree {
+    Node root; // represents the top-most node in the tree
+
     class Node {
         int value;
         Node left;
