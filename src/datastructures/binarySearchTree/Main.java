199c815c099f00e6a624981905e57009c34fe239
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-31 15:09:11 +0545

    test contains method

--- src/datastructures/binarySearchTree/Main.java
+++ src/datastructures/binarySearchTree/Main.java
@@ -4,22 +4,14 @@ public class Main {
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
 
-        System.out.println(bst.insert(27)); // true
-        System.out.println(bst.insert(55)); // true
-        System.out.println(bst.insert(17)); // true
-        System.out.println(bst.insert(83)); // true
-        System.out.println(bst.insert(28)); // true
-        System.out.println(bst.insert(55)); // false
-        System.out.println(bst.insert(25)); // true
-        System.out.println(bst.insert(12)); // true
+        bst.insert(27);
+        bst.insert(55);
+        bst.insert(17);
+        bst.insert(83);
+        bst.insert(28);
+        bst.insert(25);
+        bst.insert(12);
 
-        System.out.println("-------------------------------");
-
-        System.out.println(bst.root.left.left.value); // 12
-        System.out.println(bst.root.left.right.value); // 25
-        System.out.println(bst.root.right.left.value); // 28
-        System.out.println(bst.root.right.right.value); // 83
-        System.out.println(bst.root.left.value); // 17
-        System.out.println(bst.root.right.value); // 55
+        System.out.println(bst.contains(17));
     }
 }
