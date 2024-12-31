f73899fc1ba1472aeb3bf08224f02f7a3f42fa43
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-31 12:16:43 +0545

    create insert method (self-implementation)

--- src/datastructures/binarySearchTree/BinarySearchTree.java
+++ src/datastructures/binarySearchTree/BinarySearchTree.java
@@ -12,4 +12,40 @@ public class BinarySearchTree {
             this.value = value;
         }
     }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+
+        Node temp = root;
+
+        while (temp != null) {
+            if (newNode.value == temp.value) return false; // all the values in binary tree must be unique
+
+            if (newNode.value < temp.value) {
+               if (temp.left == null) {
+                   temp.left = newNode;
+                   temp = null;
+
+               } else {
+                   temp = temp.left;
+               }
+
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    temp = null;
+
+                } else {
+                    temp = temp.right;
+                }
+            }
+        }
+
+        return true;
+    }
 }
