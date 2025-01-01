7f6d86b261f73e6e6aa8632384b5e5b5498b6013
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-01 11:17:35 +0545

    initial commit for bst:insert

---
+++ src/codingExercises/binarySearchTree/insert/BinarySearchTree.java
@@ -0,0 +1,28 @@
+package codingExercises.binarySearchTree.insert;
+
+public class BinarySearchTree {
+
+    private Node root;
+
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    // WRITE INSERT METHOD HERE //
+    //                          //
+    //                          //
+    //                          //
+    //                          //
+    //////////////////////////////
+
+}
