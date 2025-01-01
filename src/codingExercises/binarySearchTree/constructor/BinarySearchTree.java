806cc7d079250356de9e6062a4e633f554de84a1
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-01 11:07:45 +0545

    create inner class and constructor for Node

--- src/codingExercises/binarySearchTree/constructor/BinarySearchTree.java
+++ src/codingExercises/binarySearchTree/constructor/BinarySearchTree.java
@@ -1,6 +1,16 @@
 package codingExercises.binarySearchTree.constructor;
 
 public class BinarySearchTree {
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        private Node(int value) {
+            this.value = value;
+        }
+    }
+
     public Node getRoot() {
         return root;
     }
