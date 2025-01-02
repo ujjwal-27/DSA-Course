111aac08d14ba854cb1d72afa4e021710c26ed4e
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-02 13:29:08 +0545

    create Node class and constructor

--- src/datastructures/hashTable/HashTable.java
+++ src/datastructures/hashTable/HashTable.java
@@ -1,4 +1,14 @@
 package datastructures.hashTable;
 
 public class HashTable {
+    class Node {
+        String key;
+        int value;
+        Node next;
+
+        Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
