4bb6ad1f285b4abf3fe4d8fcf4adb77b7a3951fd
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-03 14:32:10 +0545

    test get method

--- src/datastructures/hashTable/Main.java
+++ src/datastructures/hashTable/Main.java
@@ -7,11 +7,12 @@ public class Main {
 
         ht.set("programmer", 777);
         ht.set("laptop", 2277);
-        ht.set("imac", 2277);
+        ht.set("imac", 2278);
         ht.set("development", 1000);
         ht.set("coding", 100);
         ht.set("java", 1985);
 
         ht.printTable();
+        System.out.println(ht.get("laptops"));
     }
 }
