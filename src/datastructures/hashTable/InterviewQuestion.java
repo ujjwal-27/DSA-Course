10c917f1e7ebb12a46d6b926251af09019ea2e77
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-04 13:34:57 +0545

    implement checkCommonItem method

--- src/datastructures/hashTable/InterviewQuestion.java
+++ src/datastructures/hashTable/InterviewQuestion.java
@@ -1,7 +1,28 @@
 package datastructures.hashTable;
 
+import java.util.HashMap;
+
 public class InterviewQuestion {
     public static void main(String[] args) {
-        
+        int[] a1 = {1, 3, 5};
+        int[] a2 = {2, 4, 6};
+
+        InterviewQuestion iq = new InterviewQuestion();
+        boolean result = iq.commonItem(a1, a2);
+        System.out.println(result);
+    }
+
+    public boolean checkCommonItem(int[] a1, int[] a2) {
+        HashMap<Integer, Boolean> hashMap = new HashMap<>();
+
+        for (int i = 0; i < a1.length; i++) {
+            hashMap.put(a1[i], true);
+        }
+
+        for (int i = 0; i < a2.length; i++) {
+            if (hashMap.get(a2[i]) != null) return true;
+        }
+
+        return false;
     }
 }
