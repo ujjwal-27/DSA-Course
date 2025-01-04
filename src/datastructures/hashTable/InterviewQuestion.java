7a2b34b08cbf9e71cbbe87b800f0428c2ab28c81
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-04 13:38:30 +0545

    add comments

--- src/datastructures/hashTable/InterviewQuestion.java
+++ src/datastructures/hashTable/InterviewQuestion.java
@@ -12,6 +12,14 @@ public class InterviewQuestion {
         System.out.println(result);
     }
 
+    /**
+     * There are two ways of checking whether two arrays have common items or not.
+     * One approach would be using nested for-loop which would result in O(n^2) - worst case.
+     * Another approach is implemented in this method below, which is by using hash map. This approach results in O(2n) i.e. O(n) - average case.
+     * @param a1 Array of integers
+     * @param a2 Array of integers
+     * @return Boolean true/false
+     */
     public boolean checkCommonItem(int[] a1, int[] a2) {
         HashMap<Integer, Boolean> hashMap = new HashMap<>();
 
