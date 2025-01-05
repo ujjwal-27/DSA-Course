99b33626db469884da69291f2e585bc0de283c77
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-05 15:48:29 +0545

    initial commit for coding exercise: 54

---
+++ src/interviewQuestions/hashTable/firstNonRepeatingCharacter/Main.java
@@ -0,0 +1,32 @@
+package interviewQuestions.hashTable.firstNonRepeatingCharacter;
+
+import java.util.*;
+
+
+public class Main {
+
+    // WRITE FIRSTNONREPEATINGCHAR METHOD HERE //
+    //                                         //
+    //                                         //
+    //                                         //
+    //                                         //
+    /////////////////////////////////////////////
+
+
+    public static void main(String[] args) {
+        System.out.println(firstNonRepeatingChar("leetcode"));
+        System.out.println(firstNonRepeatingChar("hello"));
+        System.out.println(firstNonRepeatingChar("aabbcc"));
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            l
+            h
+            null
+
+        */
+
+    }
+
+}
