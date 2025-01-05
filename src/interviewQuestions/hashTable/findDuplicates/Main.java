1d6f3982476bff1805e299b7b29240fb932c625a
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-05 14:53:53 +0545

    implement findDuplicates method (hint-based)

--- src/interviewQuestions/hashTable/findDuplicates/Main.java
+++ src/interviewQuestions/hashTable/findDuplicates/Main.java
@@ -10,28 +10,56 @@ public class Main {
      * @param nums Array of integers
      * @return Duplicate list
      */
+//    public static List<Integer> findDuplicates(int[] nums) {
+//        HashMap<Integer, Boolean> hashMap = new HashMap<>(); // HashMap for storing values from 'nums' integer array.
+//        List<Integer> duplicates = new ArrayList<>(); // ArrayList for storing duplicate values
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            boolean flag = false;
+//
+//            // If the number from 'nums' array already exists in the map as one of the keys, then flag will be assigned to true.
+//            if (hashMap.get(nums[i]) != null) {
+//                flag = true;
+//            }
+//
+//            // The 'put' method inserts the key-value pair into the map if the key does not exist in the map, else it updates the value associated with the key.
+//            // If the key does not exist in the map, then the corresponding value of the key will be inserted as 'false'
+//            // If the key exists in the map, then the corresponding value of the key will be updated to 'true' as per above if-condition.
+//            hashMap.put(nums[i], flag);
+//        }
+//
+//        // using loop in the final hash map
+//        hashMap.forEach((key , value) -> {
+//            // if the value is true, add the 'keys' (integers) to the ArrayList: duplicate
+//            if (value) {
+//                duplicates.add(key);
+//            }
+//        });
+//
+//        return duplicates;
+//    }
+
+    /**
+     * Solution based on 'hints' provided in the course. The original solution can be found in the course.
+     * @param nums Array of integers
+     * @return Duplicate list
+     */
     public static List<Integer> findDuplicates(int[] nums) {
-        HashMap<Integer, Boolean> hashMap = new HashMap<>(); // HashMap for storing values from 'nums' integer array.
+        HashMap<Integer, Integer> numCounts = new HashMap<>(); // HashMap for storing values from 'nums' integer array.
         List<Integer> duplicates = new ArrayList<>(); // ArrayList for storing duplicate values
 
-        for (int i = 0; i < nums.length; i++) {
-            boolean flag = false;
+        for (int num: nums) {
+            int count = 1;
 
-            // If the number from 'nums' array already exists in the map as one of the keys, then flag will be assigned to true.
-            if (hashMap.get(nums[i]) != null) {
-                flag = true;
+            if (numCounts.get(num) != null) {
+                count++;
             }
 
-            // The 'put' method inserts the key-value pair into the map if the key does not exist in the map, else it updates the value associated with the key.
-            // If the key does not exist in the map, then the corresponding value of the key will be inserted as 'false'
-            // If the key exists in the map, then the corresponding value of the key will be updated to 'true' as per above if-condition.
-            hashMap.put(nums[i], flag);
+            numCounts.put(num, count);
         }
 
-        // using loop in the final hash map
-        hashMap.forEach((key , value) -> {
-            // if the value is true, add the 'keys' (integers) to the ArrayList: duplicate
-            if (value) {
+        numCounts.forEach((key, value) -> {
+            if (value > 1) {
                 duplicates.add(key);
             }
         });
