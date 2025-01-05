7135fa11e3a4d04bd2e802e003182ed9e856cc33
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-05 13:13:43 +0545

    initial commit  for findDuplicates method

---
+++ src/interviewQuestions/hashTable/findDuplicates/Main.java
@@ -0,0 +1,36 @@
+package interviewQuestions.hashTable.findDuplicates;
+
+import java.util.*;
+
+
+import java.util.*;
+
+
+public class Main {
+
+    // WRITE THE FINDDUPLICATES METHOD HERE //
+    //                                      //
+    //                                      //
+    //                                      //
+    //                                      //
+    //////////////////////////////////////////
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 2, 1, 4, 5, 4};
+        List<Integer> duplicates = findDuplicates(nums);
+        System.out.println(duplicates);
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+			[1, 2, 4]
+
+        */
+
+    }
+
+}
+
+
