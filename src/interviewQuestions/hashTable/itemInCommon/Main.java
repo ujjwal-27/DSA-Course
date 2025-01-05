2d87e5b712a48c840247001026aa564126b7e81c
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-05 11:40:36 +0545

    implement itemInCommon method

--- src/interviewQuestions/hashTable/itemInCommon/Main.java
+++ src/interviewQuestions/hashTable/itemInCommon/Main.java
@@ -3,13 +3,19 @@ package interviewQuestions.hashTable.itemInCommon;
 import java.util.HashMap;
 
 public class Main {
+    public static boolean itemInCommon(int[] array1, int[] array2) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>(); // The second param in the generics which requires value can be of type either boolean or integer.
 
-    // WRITE THE ITEMINCOMMON METHOD HERE //
-    //                                    //
-    //                                    //
-    //                                    //
-    //                                    //
-    ////////////////////////////////////////
+        for (int i = 0; i < array1.length; i++) {
+            hashMap.put(array1[i], 0);
+        }
+
+        for (int i = 0; i < array2.length; i++) {
+            if (hashMap.get(array2[i]) != null) return true; // Main logic to determine if there is common item between the given arrays or not.
+        }
+
+        return false;
+    }
 
 
     public static void main(String[] args) {
