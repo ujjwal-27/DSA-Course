c5c46b7df948c0981b39ffefe25a374e705c33df
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-05 16:13:10 +0545

    add comments and make minor updates

--- src/interviewQuestions/hashTable/firstNonRepeatingCharacter/Main.java
+++ src/interviewQuestions/hashTable/firstNonRepeatingCharacter/Main.java
@@ -8,12 +8,13 @@ public class Main {
     public static Character firstNonRepeatingChar(String string) {
         HashMap<Character, Integer> charCounts = new HashMap<>();
 
-        for (int i = 0; i < string.length(); i++) {
-            // 'charCounts.getOrDefault(string.charAt(i), 0)' --> This code either gives the actual value of associated with key (string.charAt(i)) from the map, or gives zero if the key does not exist.
-            charCounts.put(string.charAt(i), charCounts.getOrDefault(string.charAt(i), 0) + 1); // + 1 to increase the count of associated key (character) on each encounter.
+        for (char i = 0; i < string.length(); i++) {
+            char character = string.charAt(i);
+            // 'charCounts.getOrDefault(character, 0)' --> This code either gives the actual value of associated with key (character) from the map, or gives zero if the key does not exist.
+            charCounts.put(character, charCounts.getOrDefault(character, 0) + 1); // + 1 to increase the count of associated key (character) on each encounter.
         }
 
-        // iterating through the input string again to check the occurrence count in sequence.
+        // Iterating through the 'input string' again to check the occurrence count of characters in sequence.
         for (int i = 0; i < string.length(); i++) {
             // As soon as the character with count 1 is found, return the respective character.
             if (charCounts.get(string.charAt(i)) == 1) {
