b967f251d37eb5efb1b40ef291cf15c2864b7fb2
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-05 16:17:25 +0545

    initial commit for coding exercise: 55

---
+++ src/interviewQuestions/hashTable/groupAnagrams/Main.java
@@ -0,0 +1,42 @@
+package interviewQuestions.hashTable.groupAnagrams;
+
+import java.util.*;
+
+
+public class Main {
+
+    // WRITE GROUPANAGRAMS METHOD HERE //
+    //                                 //
+    //                                 //
+    //                                 //
+    //                                 //
+    /////////////////////////////////////
+
+
+    public static void main(String[] args) {
+        System.out.println("1st set:");
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+
+        System.out.println("\n2nd set:");
+        System.out.println(groupAnagrams(new String[]{"abc", "cba", "bac", "foo", "bar"}));
+
+        System.out.println("\n3rd set:");
+        System.out.println(groupAnagrams(new String[]{"listen", "silent", "triangle", "integral", "garden", "ranged"}));
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            1st set:
+            [[eat, tea, ate], [tan, nat], [bat]]
+
+            2nd set:
+            [[abc, cba, bac], [foo], [bar]]
+
+            3rd set:
+            [[listen, silent], [triangle, integral], [garden, ranged]]
+
+        */
+
+    }
+
+}
