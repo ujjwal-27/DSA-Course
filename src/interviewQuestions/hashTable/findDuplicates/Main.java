a9381e663abdc79fec5e9e52afaa2b524a3bc20a
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-05 13:15:38 +0545

    implement findDuplicates method (self-implementation)

--- src/interviewQuestions/hashTable/findDuplicates/Main.java
+++ src/interviewQuestions/hashTable/findDuplicates/Main.java
@@ -3,22 +3,35 @@ package interviewQuestions.hashTable.findDuplicates;
 import java.util.*;
 
 
-import java.util.*;
-
-
 public class Main {
 
-    // WRITE THE FINDDUPLICATES METHOD HERE //
-    //                                      //
-    //                                      //
-    //                                      //
-    //                                      //
-    //////////////////////////////////////////
+    public static List<Integer> findDuplicates(int[] nums) {
+        HashMap<Integer, Boolean> hashMap = new HashMap<>();
+        List<Integer> duplicates = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            boolean flag = false;
+
+            if (hashMap.get(nums[i]) != null) {
+                flag = true;
+            }
+
+            hashMap.put(nums[i], flag);
+        }
+
+        hashMap.forEach((key , value) -> {
+            if (value) {
+                duplicates.add(key);
+            }
+        });
+
+        return duplicates;
+    }
 
 
     public static void main(String[] args) {
 
-        int[] nums = {1, 2, 3, 2, 1, 4, 5, 4};
+        int[] nums = {1, 2, 3, 4, 5};
         List<Integer> duplicates = findDuplicates(nums);
         System.out.println(duplicates);
 
@@ -32,5 +45,4 @@ public class Main {
     }
 
 }
-
 
