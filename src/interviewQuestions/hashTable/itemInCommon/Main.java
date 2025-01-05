94e4aa068e8732ba59e6607ec85bcacae4f1e5e2
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-05 11:23:08 +0545

    initial commit  for itemInCommon method

---
+++ src/interviewQuestions/hashTable/itemInCommon/Main.java
@@ -0,0 +1,31 @@
+package interviewQuestions.hashTable.itemInCommon;
+
+import java.util.HashMap;
+
+public class Main {
+
+    // WRITE THE ITEMINCOMMON METHOD HERE //
+    //                                    //
+    //                                    //
+    //                                    //
+    //                                    //
+    ////////////////////////////////////////
+
+
+    public static void main(String[] args) {
+
+        int[] array1 = {1, 3, 5};
+        int[] array2 = {2, 4, 5};
+
+        System.out.println(itemInCommon(array1, array2));
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            true
+
+        */
+
+    }
+
+}
