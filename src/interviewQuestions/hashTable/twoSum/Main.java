c7cc271b466b1c31b2c1bae70f66876ec264397f
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-06 15:44:22 +0545

    initial commit for twoSum method

---
+++ src/interviewQuestions/hashTable/twoSum/Main.java
@@ -0,0 +1,41 @@
+package interviewQuestions.hashTable.twoSum;
+
+import java.util.*;
+
+
+public class Main {
+
+    // WRITE THE TWOSUM MEHTOD HERE //
+    //                              //
+    //                              //
+    //                              //
+    //                              //
+    //////////////////////////////////
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 3}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3, 4, 5}, 10)));
+        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3, 4, 5}, 7)));
+        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3, 4, 5}, 3)));
+        System.out.println(Arrays.toString(twoSum(new int[]{}, 0)));
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            [0, 1]
+            [1, 2]
+            [0, 1]
+            []
+            [2, 3]
+            [0, 1]
+            []
+
+        */
+
+    }
+
+}
+
