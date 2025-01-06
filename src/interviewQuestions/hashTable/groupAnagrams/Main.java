197ec205698763586d4b32481a28f89cd50a070c
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-06 15:18:35 +0545

    add comments and make minor changes

--- src/interviewQuestions/hashTable/groupAnagrams/Main.java
+++ src/interviewQuestions/hashTable/groupAnagrams/Main.java
@@ -6,31 +6,44 @@ import java.util.*;
 public class Main {
 
     public static List<List<String>> groupAnagrams(String[] strings) {
+        // This HashMap is used to store anagramsKey as key, and list of anagrams as value.
+        // Example:
+        // For 1st set - {aet=[eat, tea, ate], abt=[bat], ant=[tan, nat]}
+        // For 2nd set - {abr=[bar], abc=[abc, cba, bac], foo=[foo]}
+        // For 3rd set - {eilnst=[listen, silent], aegilnrt=[triangle, integral], adegnr=[garden, ranged]}
         HashMap<String, ArrayList<String>> anagramGroups = new HashMap<>();
-        List<List<String>> arrayList = new ArrayList<>(); // dummy array list to return something in the end to avoid error
+
+        // ArrayList to store list of anagram's list.
+        List<List<String>> anagramList = new ArrayList<>();
 
         for (String string: strings) {
-            char[] chars = string.toCharArray();
-            Arrays.sort(chars);
+            char[] chars = string.toCharArray(); // Converting and storing current string's characters to array
+            Arrays.sort(chars); // Sorting array of characters
+            String anagramsKey = new String(chars); // Converting sorted array of characters to string. This 'anagramsKey' will be used as 'key' in hash map, and the 'list of its associated string' will be added as 'value'.
 
-            String canonical = new String(chars);
-
-            if (anagramGroups.containsKey(canonical)) {
-                anagramGroups.get(canonical).add(string);
+            if (anagramGroups.containsKey(anagramsKey)) { // Check if the key already exists in the map.
+                anagramGroups.get(anagramsKey).add(string); // If yes, add the 'string' to the 'list of anagrams' associated with the key (anagramsKey).
 
             } else {
+                // If the key doesn't exist, first create a new ArrayList, and add the string to the list.
+                // Then, add the key-value pairs of anagramsKey and group (ArrayList) to the map.
                 ArrayList<String> group = new ArrayList<>();
                 group.add(string);
 
-                anagramGroups.put(canonical, group);
+                anagramGroups.put(anagramsKey, group);
             }
         }
 
+//        System.out.println(anagramGroups); // Check how canonical as key, and list of anagrams as value is stored in the HashMap
+
+        // Populating the values (list of anagrams) from the map to the ArrayList
         anagramGroups.forEach((key, value) -> {
-            arrayList.add(value);
+            anagramList.add(value);
         });
 
-        return arrayList;
+        return anagramList;
+
+//        return new ArrayList<>(anagramGroups.values()) // OR directly return this line of code
     }
 
 
