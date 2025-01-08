2a920bb517a188850e99ca6be35833929a1abbf1
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-08 16:06:09 +0545

    initial commit for hasUniqueChars method

---
+++ src/interviewQuestions/hashTable/setHasUniqueChars/Main.java
@@ -0,0 +1,37 @@
+package interviewQuestions.hashTable.setHasUniqueChars;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Main {
+
+    // WRITE HASUNIQUECHARS METHOD HERE //
+    //                                  //
+    //                                  //
+    //                                  //
+    //                                  //
+    //////////////////////////////////////
+
+
+    public static void main(String[] args) {
+        System.out.println(hasUniqueChars("abcdefg")); // should return true
+        System.out.println(hasUniqueChars("hello")); // should return false
+        System.out.println(hasUniqueChars("")); // should return true
+        System.out.println(hasUniqueChars("0123456789")); // should return true
+        System.out.println(hasUniqueChars("abacadaeaf")); // should return false
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            true
+            false
+            true
+            true
+            false
+
+        */
+
+    }
+
+}
+
