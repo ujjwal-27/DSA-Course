152010a0cb10a73bc4918e50b95b744829c0c10a
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-08 16:46:50 +0545

    add comments

--- src/interviewQuestions/hashTable/setFindPairs/Main.java
+++ src/interviewQuestions/hashTable/setFindPairs/Main.java
@@ -5,6 +5,19 @@ import java.util.*;
 
 public class Main {
 
+    /**
+     * First, create an empty HashSet (intSet) and ArrayList (pairList).
+     * Then, add all the integers from 'arr1' to 'intSet' using for-loop.
+     * Then, using for-loop, find the 'difference' between 'target value' and 'integers' of arr2 on each iteration.
+     * On each iteration, check if the 'difference' value is present in 'intSet' or not.
+     * If yes, then the 'difference' value and respective 'integer' from 'arr2' is a pair which sums to 'target' value.
+     * Create an array (pairArr) of those two pair integers, and add the 'array' to 'pairList'.
+     * Lastly, return 'pairList'.
+     * @param arr1 Array of integers
+     * @param arr2 Array of integers
+     * @param target Integer
+     * @return List of integer array
+     */
     public static List<int[]> findPairs(int[] arr1, int[] arr2, int target) {
         Set<Integer> intSet = new HashSet<>();
         List<int[]> pairList = new ArrayList<>();
