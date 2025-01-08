a6d88d917e3f2fb87cdd5e5b3395872acb96aa53
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-08 13:30:34 +0545

    create a HashSet obj, and add values

--- src/interviewQuestions/hashTable/setIntroduction/Main.java
+++ src/interviewQuestions/hashTable/setIntroduction/Main.java
@@ -1,4 +1,22 @@
 package interviewQuestions.hashTable.setIntroduction;
 
+import java.util.HashSet;
+
 public class Main {
+    public static void main(String[] args) {
+        setIntro();
+    }
+
+    public static void setIntro() {
+        HashSet<Integer> s1 = new HashSet<>();
+
+        s1.add(1);
+        s1.add(2);
+        s1.add(3);
+        s1.add(4);
+        s1.add(5);
+        System.out.println(s1);
+
+        
+    }
 }
