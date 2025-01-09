8bca19a16f725e9235b15924fe157b596e7035a3
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-09 12:53:55 +0545

    add comments

--- src/interviewQuestions/hashTable/setLongestConsecutiveSequence/Main.java
+++ src/interviewQuestions/hashTable/setLongestConsecutiveSequence/Main.java
@@ -6,6 +6,19 @@ import java.util.Set;
 
 public class Main {
 
+    /**
+     * The core concept implemented in this method is:
+     * First, populate all the integers to an empty set 'numSet'. This will remove all the duplicate value from the array.
+     * Initialize a variable 'longestSequence' with 0, to calculate the longest sequence in 'numSet'.
+     * Then, execute advanced for-loop on 'numSet', and do following for each integer 'num':
+     * - Check if (num - 1) exists or not in 'numSet'. If exists, move to another number in next iteration because the existence of (num - 1) reflects that current 'num' is already part of a 'sequence' which begin from another number less than 'num'.
+     * - If it doesn't exist, initialize two variables 'currentNum' assigned with value of 'num', and 'currentSequence' assigned with 1.
+     * - Then, while there exists (currentNum + 1) in numSets, increment both currentNum and currentSequence by 1. This will compute any 'sequence' present in the 'numSet'.
+     * - After that, compare 'longestSequence' and 'currentSequence' using 'Math.max()' method, and assign the max value to 'longestSequence',
+     * Finally, return 'longestSequence'.
+     * @param nums Array of integers
+     * @return [Integer] Longest sequence in array 'nums'.
+     */
     public static int longestConsecutiveSequence(int[] nums) {
         Set<Integer> numSet = new HashSet<>();
 
@@ -15,6 +28,17 @@ public class Main {
 
         int longestSequence = 0;
 
+        /**
+         * Here, instead of 'advanced for-loop', we might think of using 'forEach loop' which implements 'lambda expression': 'numSet.forEach((num) -> {})'.
+         * But, there are certain rules that need to be considered before using lambda expression.
+         * The logic inside the loop requires modification of 'longestSequence' which is a local variable.
+         * Inside lambda expression, modification of local variables is never possible as the compiler will throw an error.
+         * A local variable must be declared 'final' before using it inside lambda expression.
+         * To modify values of variable in lambda, use:
+         * - An array or object as a wrapper for the value.
+         * - Instance variable in which the modification rules does not imply.
+         * - Atomic variables for thread-safe numeric operations.
+         */
         for (int num: numSet) {
             if (!numSet.contains(num - 1)) {
                 int currentNum = num;
