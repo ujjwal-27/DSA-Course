d694bee8ecadf55a2ff4c74f4d3370c6c8aea117
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-15 14:13:25 +0545

    implement addVertex method

--- src/datastructures/graph/Graph.java
+++ src/datastructures/graph/Graph.java
@@ -5,4 +5,13 @@ import java.util.HashMap;
 
 public class Graph {
     private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public boolean addVertex(String vertex) {
+        if (adjList.get(vertex) != null) {
+            adjList.put(vertex, new ArrayList<>());
+            return true;
+        }
+
+        return false;
+    }
 }
