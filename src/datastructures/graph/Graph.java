84473a2cc49bc2cb089f4ceadcdf744a6ce3dd2c
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-15 14:15:19 +0545

    test methods

--- src/datastructures/graph/Graph.java
+++ src/datastructures/graph/Graph.java
@@ -7,7 +7,7 @@ public class Graph {
     private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
 
     public boolean addVertex(String vertex) {
-        if (adjList.get(vertex) != null) {
+        if (adjList.get(vertex) == null) {
             adjList.put(vertex, new ArrayList<>());
             return true;
         }
