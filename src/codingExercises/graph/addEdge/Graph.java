7480a463becd9bec2eed923e51b9107498c6d3be
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-16 14:09:21 +0545

    initial commit for addEdge method

---
+++ src/codingExercises/graph/addEdge/Graph.java
@@ -0,0 +1,34 @@
+package codingExercises.graph.addEdge;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Graph {
+
+    private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public HashMap<String, ArrayList<String>> getAdjList() {
+        return adjList;
+    }
+
+    public void printGraph() {
+        System.out.println(adjList);
+    }
+
+    public boolean addVertex(String vertex) {
+        if (adjList.get(vertex) == null) {
+            adjList.put(vertex, new ArrayList<String>());
+            return true;
+        }
+        return false;
+    }
+
+    // WRITE ADDEDGE METHOD HERE //
+    //                           //
+    //                           //
+    //                           //
+    //                           //
+    ///////////////////////////////
+
+}
