c450a79cf3c7ba7c04904d1a13c759085ce77d25
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-16 14:30:20 +0545

    implement removeVertex method

--- src/codingExercises/graph/removeVertex/Graph.java
+++ src/codingExercises/graph/removeVertex/Graph.java
@@ -42,11 +42,15 @@ public class Graph {
         return false;
     }
 
-    // WRITE REMOVEVERTEX METHOD HERE //
-    //                                //
-    //                                //
-    //                                //
-    //                                //
-    ////////////////////////////////////
+    public boolean removeVertex(String vertex) {
+        if (adjList.get(vertex) == null) return false;
+
+        for (String otherVertex: adjList.get(vertex)) {
+            adjList.get(otherVertex).remove(vertex);
+        }
+
+        adjList.remove(vertex);
+        return true;
+    }
 
 }
