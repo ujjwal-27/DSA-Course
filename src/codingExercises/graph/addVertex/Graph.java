f0e69cf44669c4fde504661440157cef64c9b234
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-16 14:02:41 +0545

    initial commit for addVertex method

---
+++ src/codingExercises/graph/addVertex/Graph.java
@@ -0,0 +1,26 @@
+package codingExercises.graph.addVertex;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Graph {
+
+    private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public HashMap<String, ArrayList<String>> getAdjList() {
+        return adjList;
+    }
+
+    public void printGraph() {
+        System.out.println(adjList);
+    }
+
+    // WRITE ADDVERTEX METHOD HERE //
+    //                             //
+    //                             //
+    //                             //
+    //                             //
+    /////////////////////////////////
+
+}
