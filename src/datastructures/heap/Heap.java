0054d689cb770d140a567e0a5b4382e29cf9cd25
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-17 13:21:56 +0545

    create heap constructor

--- src/datastructures/heap/Heap.java
+++ src/datastructures/heap/Heap.java
@@ -1,4 +1,12 @@
 package datastructures.heap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Heap {
+    private List<Integer> heap;
+
+    public Heap() {
+        this.heap = new ArrayList<>();
+    }
 }
