5f3361941b838cfe5d80acc4267483a08879243a
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-22 11:33:39 +0545

    initial commit for heap:insert

---
+++ src/codingExercises/heap/insert/Heap.java
@@ -0,0 +1,49 @@
+package codingExercises.heap.insert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Heap {
+    private List<Integer> heap;
+
+    public Heap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+
+    // WRITE THE INSERT METHOD HERE //
+    //                              //
+    //                              //
+    //                              //
+    //                              //
+    //////////////////////////////////
+
+}
+
+
+
+
+
+
+
