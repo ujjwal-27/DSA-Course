8485d53e585fda2c4e4bb9041f63d630a6861624
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-22 13:37:55 +0545

    test sinkDown method

--- src/codingExercises/heap/sinkDown/Main.java
+++ src/codingExercises/heap/sinkDown/Main.java
@@ -11,7 +11,8 @@ public class Main {
         myHeap.insert(55);
         myHeap.insert(60);
         myHeap.insert(50);
-//        myHeap.insert(79);
+        myHeap.insert(65);
+        myHeap.insert(79);
 
         System.out.println(myHeap.getHeap());
 
