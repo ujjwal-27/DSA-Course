3f2f01dd2a9620add3ae99b783897363add48a7f
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-22 14:50:32 +0545

    initial commit for minHeap:sinkDown

---
+++ src/codingExercises/heap/minHeap/sinkDown/MinHeap.java
@@ -0,0 +1,69 @@
+package codingExercises.heap.minHeap.sinkDown;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinHeap {
+    private List<Integer> heap;
+
+    public MinHeap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+
+    public void insert(int value) {
+        heap.add(value);
+        int current = heap.size() - 1;
+
+        while (current > 0 && heap.get(current) < heap.get(parent(current))) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    // WRITE THE SINKDOWN METHOD HERE //
+    //                                //
+    //                                //
+    //                                //
+    //                                //
+    ////////////////////////////////////
+
+
+    public Integer remove() {
+        if (heap.size() == 0) {
+            return null;
+        }
+
+        if (heap.size() == 1) {
+            return heap.remove(0);
+        }
+
+        int minValue = heap.get(0);
+        heap.set(0, heap.remove(heap.size() - 1));
+        sinkDown(0);
+
+        return minValue;
+    }
+
+}
