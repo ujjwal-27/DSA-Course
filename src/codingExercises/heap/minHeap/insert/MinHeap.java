8920122b3c6b5939c68949a124a64c9f78d8df48
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-22 14:07:03 +0545

    initial commit for minHeap:insert

---
+++ src/codingExercises/heap/minHeap/insert/MinHeap.java
@@ -0,0 +1,43 @@
+package codingExercises.heap.minHeap.insert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinHeap {
+    private List<Integer> heap;
+
+    public MinHeap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+
+    // WRITE THE INSERT METHOD HERE //
+    //                              //
+    //                              //
+    //                              //
+    //                              //
+    //////////////////////////////////
+
+}
+
