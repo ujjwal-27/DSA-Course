65878933599158c9f92b7f5c939cd368aa87de8b
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-24 14:43:00 +0545

    add comments

--- src/misc/recursion/Factorial.java
+++ src/misc/recursion/Factorial.java
@@ -6,9 +6,14 @@ public class Factorial {
         System.out.println(result);
     }
 
+    /**
+     * Here, the factorial of 'n' is computed using recursion concept.
+     * @param n An integer
+     * @return [Integer] Factorial of 'n'
+     */
     public static int getFactorial(int n) {
-        if (n == 1) return 1;
+        if (n == 1) return 1; // base case
 
-        return n * getFactorial(n - 1);
+        return n * getFactorial(n - 1); // recursive case
     }
 }
