a9228efb6e783be0465e2592eb59ffc80920ac34
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-30 13:19:42 +0545

    initial commit for rbst:ce:74

---
+++ src/codingExercises/binarySearchTree/recursive/contains/Main.java
@@ -0,0 +1,38 @@
+package codingExercises.binarySearchTree.recursive.contains;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+
+        System.out.println("BST Contains 27:");
+        System.out.println(myBST.rContains(27));
+
+        System.out.println("\nBST Contains 17:");
+        System.out.println(myBST.rContains(17));
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            BST Contains 27:
+            true
+
+            BST Contains 17:
+            false
+
+        */
+
+    }
+
+}
