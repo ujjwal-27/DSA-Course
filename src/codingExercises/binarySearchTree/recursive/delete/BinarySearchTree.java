66d159f440a3dc47b0a7cc61a6d458215f9c0a62
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-30 15:34:37 +0545

    initial commit for rbst delete (ce:77)

---
+++ src/codingExercises/binarySearchTree/recursive/delete/BinarySearchTree.java
@@ -0,0 +1,112 @@
+package codingExercises.binarySearchTree.recursive.delete;
+
+public class BinarySearchTree {
+
+    private Node root;
+
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+
+    public boolean contains(int value) {
+        if (root == null) return false;
+        Node temp = root;
+        while (temp != null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private boolean rContains(Node currentNode, int value) {
+        if (currentNode == null) return false;
+
+        if (currentNode.value == value) return true;
+
+        if (value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        } else {
+            return rContains(currentNode.right, value);
+        }
+    }
+    public boolean rContains(int value) { return rContains(root, value); }
+
+
+    private Node rInsert(Node currentNode, int value) {
+        if (currentNode == null) return new Node(value);
+
+        if (value < currentNode.value) {
+            currentNode.left = rInsert(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
+    }
+    public void rInsert(int value) {
+        if (root == null) root = new Node(value);
+        rInsert(root, value);
+    }
+
+
+    public int minValue(Node currentNode) {
+        while (currentNode.left != null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+
+    // WRITE THE DELETENODE METHOD HERE //
+    //                                  //
+    //                                  //
+    //                                  //
+    //                                  //
+    //////////////////////////////////////
+
+    public void deleteNode(int value) {
+        root = deleteNode(root, value);
+    }
+}
+
+
