ce936117fbf9c0943b50b0f4192746416207a7d6
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-02 15:56:18 +0545

    add comments

--- src/interviewQuestions/recursiveBinarySearchTree/invertBinaryTree/BinarySearchTree.java
+++ src/interviewQuestions/recursiveBinarySearchTree/invertBinaryTree/BinarySearchTree.java
@@ -37,20 +37,34 @@ public class BinarySearchTree {
         insert(root, value);
     }
 
+    /**
+     * This method converts Binary Search Tree to ArrayList using Queue Linked List.
+     * As 'queue' follows 'FIFO (First In First Out)' principle, it always removes the nodes at the front (in the beginning) of the 'queue'.
+     * Initially, the 'currentNode' (root node) is added to the 'queue'.
+     * Then, a while-loop is executed until the size of 'queue' is greater than 0:
+     * - On each iteration, 'first node' from the 'queue' is removed and assigned to 'currentNode'.
+     * - If the 'currentNode' is not null, the value of 'currentNode' will be added to 'ArrayList' (results).
+     * - Then, the left and right child of the 'currentNode' will be added to 'queue'.
+     * - There will be a condition when 'currentNode' will be 'null'. This happens when the iteration reaches the point when the left and right child (null) of leaf node is added to queue.
+     * - The children of leaf node is always null, which why it is called leaf node.
+     * - That 'null' value is just removed from the 'queue', and won't be added to ArrayList (results) since it won't satisfy the if-condition (currentNode != null).
+     * Understanding the flow of this method with the help of diagram will make it more comprehensible.
+     * @return [ArrayList] List of integers derived from BST.
+     */
     public ArrayList<Integer> BFS() {
-        Node currentNode = root;
-        Queue<Node> queue = new LinkedList<>();
-        ArrayList<Integer> results = new ArrayList<>();
+        Node currentNode = root; // assigning current node with the root node of tree.
+        Queue<Node> queue = new LinkedList<>(); // declaring queue
+        ArrayList<Integer> results = new ArrayList<>(); // This is where all the node values from BST will be stored.
         if (currentNode != null) {
-            queue.add(currentNode);
+            queue.add(currentNode); // Initially adding currentNode in queue.
         }
 
         while (queue.size() > 0) {
-            currentNode = queue.remove();
+            currentNode = queue.remove(); // This removes first node from queue, and stores it in currentNode variable.
             if (currentNode != null) {
-                results.add(currentNode.value);
-                queue.add(currentNode.left);
-                queue.add(currentNode.right);
+                results.add(currentNode.value); // The value of currentNode is then added in the list (results).
+                queue.add(currentNode.left); // The left node of currentNode is added to queue
+                queue.add(currentNode.right); // The right node of currentNode is added to queue
             }
         }
         return results;
@@ -60,8 +74,15 @@ public class BinarySearchTree {
         root = invertTree(root);
     }
 
+    /**
+     * The best way to understand the working of this method is by 'drawing diagrams' of step by step implementations.
+     * The diagram of 'call stack' will help the most, and track the 'recursion' how it's taking place.
+     * The comments in the 'solution explanation' of this question is also really helpful.
+     * @param node [Node]
+     * @return [Node] Root node
+     */
     private Node invertTree(Node node) {
-        if (node == null) return null;
+        if (node == null) return null; // base case
 
         Node temp = node.left;
         node.left = invertTree(node.right);
