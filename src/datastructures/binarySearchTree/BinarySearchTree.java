080e7ffe393fad86106c1527e4735de6b7dab0cf
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-03 12:36:20 +0545

    implement BFS method

--- src/datastructures/binarySearchTree/BinarySearchTree.java
+++ src/datastructures/binarySearchTree/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package datastructures.binarySearchTree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
     Node root; // represents the top-most node in the tree
 
@@ -11,6 +15,30 @@ public class BinarySearchTree {
         private Node(int value) {
             this.value = value;
         }
+    }
+
+    public ArrayList<Integer> BFS() {
+        Node currentNode = root;
+
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        queue.add(currentNode);
+
+        while (queue.size() > 0) {
+            currentNode = queue.remove();
+            result.add(currentNode.value);
+
+            if (currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+
+            if (currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+
+        return result;
     }
 
     public boolean contains(int value) {
