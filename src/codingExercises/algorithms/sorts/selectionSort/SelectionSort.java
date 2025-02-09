1a9248461d329415eee76510fcf7b31f14ac5fb6
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-09 15:21:09 +0545

    implement selectionSort (self implementation)

---
+++ src/codingExercises/algorithms/sorts/selectionSort/SelectionSort.java
@@ -0,0 +1,36 @@
+package codingExercises.algorithms.sorts.selectionSort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] numbers = {4, 2, 6, 5, 1, 3};
+        selectionSort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    /**
+     *
+     * @param numbers
+     */
+    public static void selectionSort(int[] numbers) {
+        int minIndex = 0;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < numbers[minIndex] ) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = numbers[i];
+                numbers[i] = numbers[minIndex];
+                numbers[minIndex] = temp;
+            }
+
+            minIndex = i + 1;
+        }
+    }
+}
