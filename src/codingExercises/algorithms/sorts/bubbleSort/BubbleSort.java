6c0b2726a4e955da7a21617fafdb4f4ae1175782
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-09 12:09:30 +0545

    initial commit

---
+++ src/codingExercises/algorithms/sorts/bubbleSort/BubbleSort.java
@@ -0,0 +1,12 @@
+package codingExercises.algorithms.sorts.bubbleSort;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] numbers = {4, 2, 6, 5, 1, 3};
+        bubbleSort(numbers);
+     }
+
+    public static void bubbleSort(int[] numbers) {
+        
+    }
+}
