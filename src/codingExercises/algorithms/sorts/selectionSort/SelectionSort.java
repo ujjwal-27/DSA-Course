199efdf5e983168e856aa0dab6599723dda5575f
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-09 16:12:39 +0545

    add comments

--- src/codingExercises/algorithms/sorts/selectionSort/SelectionSort.java
+++ src/codingExercises/algorithms/sorts/selectionSort/SelectionSort.java
@@ -11,8 +11,14 @@ public class SelectionSort {
     }
 
     /**
-     *
-     * @param numbers
+     * This algorithm implements nested for-loop.
+     * The outer-loop starts from 0-index till 'second last' index of array.
+     * - On every iteration of 'outer-loop', the variable 'minIndex' is created and initialized with the value of 'i'.
+     * The 'outer-loop' starts from (i + 1) index till the 'last' index of array.
+     * - On every iteration of 'inner-loop', the value at 'j-index' is compared with value at 'minIndex'.
+     * - If value at 'j-index' is less than value at 'minIndex', the 'minIndex' is assigned with 'j-index'.
+     * After the complete iteration of inner-loop, if 'minIndex != i', value at 'i' and 'minIndex' is swapped.
+     * @param numbers Array of integers.
      */
     public static void selectionSort(int[] numbers) {
         for (int i = 0; i < numbers.length - 1; i++) {
