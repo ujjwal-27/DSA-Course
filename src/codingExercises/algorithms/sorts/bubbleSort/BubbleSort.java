e96b4051fa095ada9d7d341a01f011f73ef187ac
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-09 13:07:09 +0545

    add comments

--- src/codingExercises/algorithms/sorts/bubbleSort/BubbleSort.java
+++ src/codingExercises/algorithms/sorts/bubbleSort/BubbleSort.java
@@ -10,6 +10,16 @@ public class BubbleSort {
         System.out.println(Arrays.toString(numbers));
      }
 
+    /**
+     * Here, nested for-loop is used.
+     * Every adjacent elements in the array is compared with each other.
+     * Initially, this is done starting from 0-index to the last index of the array.
+     * - In next phase (iteration) of outer-loop it is done starting from 0-index to the second-last index of the array.
+     * - In each iteration (phase) of outer-loop, the index from the end is reduced by 1, until it reaches index-1.
+     * In inner loop, the comparison of adjacent elements are done.
+     * - If the left element is greater than right element, they are swapped.
+     * @param numbers [Array of integers]
+     */
     public static void bubbleSort(int[] numbers) {
         for (int i = numbers.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
