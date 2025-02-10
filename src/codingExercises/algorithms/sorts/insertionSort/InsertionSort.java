4e54e1fc3f0e631223f4c426daba8f5978d04fb6
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-10 14:49:21 +0545

    add comments

--- src/codingExercises/algorithms/sorts/insertionSort/InsertionSort.java
+++ src/codingExercises/algorithms/sorts/insertionSort/InsertionSort.java
@@ -10,6 +10,16 @@ public class InsertionSort {
         System.out.println(Arrays.toString(numbers));
     }
 
+    /**
+     * SELF IMPLEMENTATION: Using 'swap' approach.
+     * Here, 'nested loop' is implemented.
+     * In outer loop, two variables 'currentIndex' and 'previousIndex' is defined.
+     * - 'currentIndex' is assigned with current index, whereas 'previousIndex' is assigned with index before currentIndex.
+     * The inner loop is executed while 'previousIndex' is greater than and equals to '0', and while value of 'currentIndex' is less than 'previousIndex'.
+     * - Here, the value between 'previousIndex' and 'currentIndex' is swapped.
+     * - Then, both 'currentIndex' and 'previousIndex' is decremented by '1'. This is done to get track of the 'actual value' in 'currentIndex'.
+     * @param numbers Arrays of integers
+     */
     public static void insertionSort(int[] numbers) {
         for (int i = 1; i < numbers.length; i++) {
             int currentIndex = i;
