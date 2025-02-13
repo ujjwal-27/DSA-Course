d1d703a71f236553b246f19887244a7c5424c65e
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-13 12:51:23 +0545

    initial commit

---
+++ src/codingExercises/sorts/selectionSort/Main.java
@@ -0,0 +1,33 @@
+package codingExercises.sorts.selectionSort;
+
+import java.util.Arrays;
+
+
+public class Main {
+
+    // WRITE SELECTIONSORT METHOD HERE //
+    //                                 //
+    //                                 //
+    //                                 //
+    //                                 //
+    /////////////////////////////////////
+
+
+    public static void main(String[] args) {
+
+        int[] myArray = {4,2,6,5,1,3};
+
+        selectionSort(myArray);
+
+        System.out.println( Arrays.toString(myArray) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            [1, 2, 3, 4, 5, 6]
+
+         */
+
+    }
+
+}
