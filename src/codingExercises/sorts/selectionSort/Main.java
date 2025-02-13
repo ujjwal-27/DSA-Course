a279d293cd338f056ca9283c8487076df2dc0202
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-13 13:10:06 +0545

    implement selectionSort method

--- src/codingExercises/sorts/selectionSort/Main.java
+++ src/codingExercises/sorts/selectionSort/Main.java
@@ -5,12 +5,23 @@ import java.util.Arrays;
 
 public class Main {
 
-    // WRITE SELECTIONSORT METHOD HERE //
-    //                                 //
-    //                                 //
-    //                                 //
-    //                                 //
-    /////////////////////////////////////
+    public static void selectionSort(int[] numbers) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < numbers[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = numbers[i];
+                numbers[i] = numbers[minIndex];
+                numbers[minIndex] = temp;
+            }
+        }
+    }
 
 
     public static void main(String[] args) {
