779f2a7d3d5004cd815aa63cdeef4ef293a52d28
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-13 12:45:27 +0545

    implement bubbleSort method

--- src/codingExercises/sorts/bubbleSorts/Main.java
+++ src/codingExercises/sorts/bubbleSorts/Main.java
@@ -5,12 +5,17 @@ import java.util.Arrays;
 
 public class Main {
 
-    // WRITE BUBBLESORT METHOD HERE //
-    //                              //
-    //                              //
-    //                              //
-    //                              //
-    //////////////////////////////////
+    public static void bubbleSort(int[] numbers) {
+        for (int i = numbers.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+    }
 
 
     public static void main(String[] args) {
