cd343f18ded4b46a8bc1e9c6878f6e65402d4768
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-14 13:41:49 +0545

    add comments

--- src/interviewQuestions/basicSorts/bubbleSortOfLL/LinkedList.java
+++ src/interviewQuestions/basicSorts/bubbleSortOfLL/LinkedList.java
@@ -60,6 +60,20 @@ public class LinkedList {
         length++;
     }
 
+    /**
+     * This method implements 'bubble sort' approach to sort linked list. Hence, a nested loop will be implemented.
+     * Initially, a node variable 'sortedUntil' is assigned to 'null' (i.e. at the every end of the list)
+     * An outer while-loop is executed until the 'sortedUntil' is not equal to 'second node' of the list i.e. 'head.next'.
+     * - Within this outer loop, a node variable 'currentNode' is assigned with 'head' node.
+     * - Then, an inner while-loop is executed until 'currentNode.next' does not reach 'sortedUntil'.
+     * -- Within this inner loop, the value of 'currentNode' is compared with the value of the node 'next' to it (i.e. currentNode.next).
+     * -- If value of 'currentNode' is greater, 'swap' the values between the two nodes.
+     * -- After the completion of each iteration of inner loop, shift 'currentNode' to the node 'next' to it (currentNode.next).
+     * -- This is how it will be determined whether the 'currentNode.next' has reached 'sortedUntil' or not.
+     * -- Once the condition is satisfied, the inner loop is completed.
+     * - After the complete execution of inner loop, 'sortedUntil' node is assigned to the node 'before' it (i.e. currentNode).
+     * - This is how it will be determined whether the 'sortedUntil' has reached 'head.next' or not.
+     */
     public void bubbleSort() {
         if (this.length < 2) return;
 
