e0d10f1ed2e7912cf282a276fd4fbcc1d781e26e
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-05 15:48:30 +0545

    add comments

--- src/interviewQuestions/basicSorts/insertionSort/LinkedList.java
+++ src/interviewQuestions/basicSorts/insertionSort/LinkedList.java
@@ -60,6 +60,25 @@ public class LinkedList {
         length++;
     }
 
+    /**
+     * In this method, the core logic is first to create a 'sorted list' and 'unsorted list' from the 'original list'.
+     * Initially, the 'sorted list' contains only the first element from the 'original list', and rest of the elements from the 'original list' are contained in 'unsorted list'.
+     * The 'head' node of sorted list is 'sortedListHead', whereas 'head' node of unsorted list is 'unsortedListHead'.
+     * Then, a while-loop is executed on unsorted list until the 'unsortedListHead' is null. (outer loop)
+     * Here, a 'currentNode' is created and assigned to 'unsortedListHead', and 'unsortedListHead' is assigned to its 'next node' (unsortedListHead.next).
+     * Then, value of 'currentNode' is compared with value of 'sortedListHead'.
+     * - IF 'currentNode.value' is smaller than 'sortedListHead.value', then 'currentNode.next' is pointed to 'sortedListHead' and 'sortedListHead' is assigned as 'currentNode'.
+     * - ELSE, a 'searchPointer' is created and pointed to 'sortedListHead'.
+     * -- A while-loop is executed until 'searchPointer.next' is not null, and 'currentNode.value' is greater than 'searchPointer.next.value'.
+     * -- On each iteration of the while-loop, 'searchPointer' is assigned to its 'next node' (searchPointer.next).
+     * - After the completion of while-loop, 'next pointer' of 'currentNode' is pointed to next node of 'searchPointer' (currentNode.next = searchPointer.next).
+     * - And, 'next pointer' of 'searchPointer' is pointed to 'currentNode'.
+     * - This process ensures the elements in the 'sorted list' are in correct and accurate order.
+     * Once the 'unsortedListHeader' is null, this marks the complete execution of outer-loop.
+     * Then, the 'header' is assigned to the 'sortedListHeader', and a 'temp' node is traversed through the 'sorted list' until it reaches the 'last element'.
+     * Then the 'tail' is assigned to the 'last element' of the sorted list.
+     * The resulting list will give a sorted list.
+     */
     public void insertionSort() {
         if (this.length < 2) return;
 
