bb0f17c9d08ba602794f7f48987067b80207cc25
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-18 12:52:27 +0545

    initial commit for merge method

---
+++ src/algorithms/sorts/mergeSort/MergeSort.java
@@ -0,0 +1,17 @@
+package algorithms.sorts.mergeSort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] array1 = {1, 3, 7, 8};
+        int[] array2 = {2, 4, 5, 6};
+
+        int[] result = merge(array1, array2);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] merge(int[] array1, int[] array2) {
+        
+    }
+}
