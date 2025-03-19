ac7677a86a24b4d41c8aad5b73855d4153811430
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-19 12:06:47 +0545

    initial commit for merge:ce:92

---
+++ src/codingExercises/sorts/mergeSort/Main.java
@@ -0,0 +1,33 @@
+package codingExercises.sorts.mergeSort;
+
+import java.util.Arrays;
+
+public class Main {
+
+    // WRITE MERGE METHOD HERE //
+    //                         //
+    //                         //
+    //                         //
+    //                         //
+    /////////////////////////////
+
+
+    public static void main(String[] args) {
+
+        int[] array1 = {1,3,7,8};
+        int[] array2 = {2,4,5,6};
+
+        System.out.println( Arrays.toString( merge(array1, array2) ) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            [1, 2, 3, 4, 5, 6, 7, 8]
+
+         */
+
+    }
+
+}
+
+
