6814c6f6a5d3b7269bd9c32ac96fd6458b2df877
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-19 13:27:28 +0545

    initial commit for merge:ce:93

---
+++ src/codingExercises/sorts/mergeSort/codingExercise93/Main.java
@@ -0,0 +1,68 @@
+package codingExercises.sorts.mergeSort.codingExercise93;
+
+import java.util.Arrays;
+
+
+public class Main {
+
+    public static int[] merge(int[] array1, int[] array2) {
+        int[] combined = new int[array1.length + array2.length];
+        int index = 0;
+        int i = 0;
+        int j = 0;
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] < array2[j]) {
+                combined[index] = array1[i];
+                index++;
+                i++;
+            } else {
+                combined[index] = array2[j];
+                index++;
+                j++;
+            }
+        }
+        while (i < array1.length) {
+            combined[index] = array1[i];
+            index++;
+            i++;
+        }
+        while (j < array2.length) {
+            combined[index] = array2[j];
+            index++;
+            j++;
+        }
+        return combined;
+    }
+
+
+    // WRITE MERGESORT METHOD HERE //
+    //                             //
+    //                             //
+    //                             //
+    //                             //
+    /////////////////////////////////
+
+
+    public static void main(String[] args) {
+
+        int[] originalArray = {3,1,4,2};
+
+        int [] sortedArray = mergeSort(originalArray);
+
+        System.out.println( "Original Array: " + Arrays.toString( originalArray ) );
+
+        System.out.println( "\nSorted Array: " + Arrays.toString( sortedArray ) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Original Array: [3, 1, 4, 2]
+
+            Sorted Array: [1, 2, 3, 4]
+
+         */
+
+    }
+
+}
+
