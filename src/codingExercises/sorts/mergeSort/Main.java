c5bfee7d0f40fbef83ae0e59f03b69f45600f691
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-19 13:20:50 +0545

    implement merge method

--- src/codingExercises/sorts/mergeSort/Main.java
+++ src/codingExercises/sorts/mergeSort/Main.java
@@ -4,12 +4,39 @@ import java.util.Arrays;
 
 public class Main {
 
-    // WRITE MERGE METHOD HERE //
-    //                         //
-    //                         //
-    //                         //
-    //                         //
-    /////////////////////////////
+    public static int[] merge(int[] array1, int[] array2) {
+        int[] combined = new int[array1.length + array2.length];
+        int index = 0;
+        int i = 0; // for array1
+        int j = 0; // for array2
+
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] < array2[j]) {
+                combined[index] = array1[i];
+                i++;
+
+            } else {
+                combined[index] = array2[j];
+                j++;
+            }
+
+            index++;
+        }
+
+        while (i < array1.length) {
+            combined[index] = array1[i];
+            index++;
+            i++;
+        }
+
+        while (j < array2.length) {
+            combined[index] = array2[j];
+            index++;
+            j++;
+        }
+
+        return combined;
+    }
 
 
     public static void main(String[] args) {
