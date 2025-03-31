265c0922f4dae547a7a5b8eb7ad44cde10688ead
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-31 15:06:50 +0545

    implement pivot method

--- src/algorithms/sorts/quickSort/QuickSort.java
+++ src/algorithms/sorts/quickSort/QuickSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class QuickSort {
     public static void main(String[] args) {
         int[] array = {4, 6, 1, 7, 3, 2, 5};
-
+        
         int resultIndex = pivot(array, 0, array.length - 1);
 
         System.out.println("Returned index: " + resultIndex);
@@ -13,7 +13,18 @@ public class QuickSort {
     }
 
     public static int pivot(int[] array, int pivotIndex, int endIndex) {
+        int swapIndex = pivotIndex;
 
+        for (int i = pivotIndex + 1; i <= endIndex; i++) {
+            if (array[i] < array[pivotIndex]) {
+                swapIndex++;
+                swap(array, swapIndex, i);
+            }
+        }
+
+        swap(array, pivotIndex, swapIndex);
+
+        return swapIndex;
     }
 
     public static void swap(int[] array, int firstIndex, int secondIndex) {
