1098bf2b1d0b91b38a8cee245de9939addb878ef
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-31 15:12:11 +0545

    add comments

--- src/algorithms/sorts/quickSort/QuickSort.java
+++ src/algorithms/sorts/quickSort/QuickSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class QuickSort {
     public static void main(String[] args) {
         int[] array = {4, 6, 1, 7, 3, 2, 5};
-        
+
         int resultIndex = pivot(array, 0, array.length - 1);
 
         System.out.println("Returned index: " + resultIndex);
@@ -27,6 +27,12 @@ public class QuickSort {
         return swapIndex;
     }
 
+    /**
+     * This method simply swaps two elements of 'firstIndex' and 'secondIndex' respectively.
+     * @param array Array of integers
+     * @param firstIndex [Integer] Index of an element which is to be swapped with element of 'secondIndex'.
+     * @param secondIndex [Integer] Index of an element which is to be swapped with element of 'firstIndex'.
+     */
     public static void swap(int[] array, int firstIndex, int secondIndex) {
         int temp = array[firstIndex];
         array[firstIndex] = array[secondIndex];
