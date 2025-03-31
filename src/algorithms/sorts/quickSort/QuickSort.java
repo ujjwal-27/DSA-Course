3e3d824043b1dad0090e965c404bd7afe39dfc43
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-31 15:28:46 +0545

    add comments

--- src/algorithms/sorts/quickSort/QuickSort.java
+++ src/algorithms/sorts/quickSort/QuickSort.java
@@ -12,12 +12,25 @@ public class QuickSort {
         System.out.println(Arrays.toString(array));
     }
 
+    /**
+     * In this method, 'swapIndex' is moved (or increased) only when an 'element' during iteration is smaller than 'pivot element'.
+     * After that, element of 'swapIndex' and 'index-i' are swapped respectively.
+     * This ensures, a greater element (swapIndex) and smaller element (i-index) than pivot element is swapped.
+     * After the completion of iteration, all the smaller and greater elements will be arranged in a sequence.
+     * Then, the 'pivot element' and 'element of swapIndex' is swapped.
+     * This will arrange all the smaller elements to be in left sequence, and all the greater elements to be in right sequence of pivot element in the array.
+     * @param array Array of integers
+     * @param pivotIndex [Index]
+     * @param endIndex [Integer]
+     * @return The final index of pivot element, after swapping and rearrangement of the elements smaller and greater than pivot element.
+     */
     public static int pivot(int[] array, int pivotIndex, int endIndex) {
-        int swapIndex = pivotIndex;
+        int swapIndex = pivotIndex; // swap index is always initialized at pivotIndex
 
+        // The loop always starts after pivot index
         for (int i = pivotIndex + 1; i <= endIndex; i++) {
             if (array[i] < array[pivotIndex]) {
-                swapIndex++;
+                swapIndex++; // swap index moves (or increase) only when the element is smaller than pivot element.
                 swap(array, swapIndex, i);
             }
         }
