e485baacc7d74b1f312038348ed740c5eb9a6c37
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-04-03 15:35:50 +0545

    inital commit quickSort:ce:96

---
+++ src/codingExercises/sorts/quickSort/codingExercise96/Main.java
@@ -0,0 +1,59 @@
+package codingExercises.sorts.quickSort.codingExercise96;
+
+import java.util.Arrays;
+
+
+public class Main {
+
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    private static int pivot(int[] array, int pivotIndex, int endIndex) {
+        int swapIndex = pivotIndex;
+        for (int i = pivotIndex + 1; i <= endIndex; i++) {
+            if (array[i] < array[pivotIndex]) {
+                swapIndex++;
+                swap(array, swapIndex, i);
+            }
+        }
+        swap(array, pivotIndex, swapIndex);
+
+        return swapIndex;
+    }
+
+
+    // WRITE QUICKSORT HELPER METHOD HERE //
+    //                                    //
+    //                                    //
+    //                                    //
+    //                                    //
+    ////////////////////////////////////////
+
+
+    public static void quickSort(int[] array) {
+        quickSortHelper(array, 0, array.length-1);
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] myArray = {4,6,1,7,3,2,5};
+
+        quickSort(myArray);
+
+        System.out.println( Arrays.toString( myArray ) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            [1, 2, 3, 4, 5, 6, 7]
+
+         */
+
+    }
+
+}
+
