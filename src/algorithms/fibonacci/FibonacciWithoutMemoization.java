cd45c53baab6f29272a6195a52e9c5419e366f30
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-05-01 20:09:08 +0545

    add comments

--- src/algorithms/fibonacci/FibonacciWithoutMemoization.java
+++ src/algorithms/fibonacci/FibonacciWithoutMemoization.java
@@ -1,16 +1,23 @@
 package algorithms.fibonacci;
 
 public class FibonacciWithoutMemoization {
-    static  int counter = 0;
+    static int counter = 0; // 'counter' to check no. of times the method was invoked
 
     public static void main(String[] args) {
         int n = 7;
 
-        System.out.println("Fibonacci no. on " + n + "th position : " + fib(n));
+        System.out.println("Fibonacci no. on " + n + "th position : " + fib(n)); // output: n-th fibonacci number
 
-        System.out.println("No. of times method called = " + counter);
+        System.out.println("No. of times method called = " + counter); // No. of times the method was invoked through recursion
     }
 
+    /**
+     * In this method, the n-th fibonacci number is computed through recursion method.
+     * No memoization approach is implemented.
+     * Using memoization approach, no. of method calls (counter) can be reduced.
+     * @param n [Integer] n-th position
+     * @return Fibonacci of n-th position
+     */
     public static int fib(int n) {
         counter++;
 
@@ -18,6 +25,6 @@ public class FibonacciWithoutMemoization {
             return n;
         }
 
-        return fib(n - 1) + fib(n - 2);
+        return fib(n - 1) + fib(n - 2); // recursion + base case
     }
 }
