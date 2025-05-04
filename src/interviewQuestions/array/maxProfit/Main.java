a21600d2b3a22dc89b9ebc186ed2af5e793f10bc
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-05-04 11:30:42 +0545

    inital commit for ce:102

---
+++ src/interviewQuestions/array/maxProfit/Main.java
@@ -0,0 +1,44 @@
+package interviewQuestions.array.maxProfit;
+
+public class Main {
+
+    public static int maxProfit(int[] prices) {
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] prices1 = {7, 1, 5, 3, 6, 4};
+        int profit1 = maxProfit(prices1);
+        System.out.println("Test case 1: Maximum profit: " + profit1); // prints "Maximum profit: 5"
+
+        int[] prices2 = {7, 6, 5, 4, 3, 2, 1};
+        int profit2 = maxProfit(prices2);
+        System.out.println("Test case 2: Maximum profit: " + profit2); // prints "Maximum profit: 0"
+
+        int[] prices3 = {1, 2, 3, 4, 5, 6, 7};
+        int profit3 = maxProfit(prices3);
+        System.out.println("Test case 3: Maximum profit: " + profit3); // prints "Maximum profit: 6"
+
+        int[] prices4 = {3, 2, 6, 5, 0, 3};
+        int profit4 = maxProfit(prices4);
+        System.out.println("Test case 4: Maximum profit: " + profit4); // prints "Maximum profit: 4"
+
+        int[] prices5 = {2, 4, 1};
+        int profit5 = maxProfit(prices5);
+        System.out.println("Test case 5: Maximum profit: " + profit5); // prints "Maximum profit: 2"
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Test case 1: Maximum profit: 5
+            Test case 2: Maximum profit: 0
+            Test case 3: Maximum profit: 6
+            Test case 4: Maximum profit: 4
+            Test case 5: Maximum profit: 2
+        */
+
+    }
+
+}
+
